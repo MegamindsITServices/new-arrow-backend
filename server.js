919a3b12626ddf06b67e6d32a11893024691f01c
MegamindsITServices <megamindsresearch@gmail.com>
2024-06-03 22:07:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -98,9 +98,9 @@ app.get("/s", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../arrow-frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+  res.sendFile(path.join(__dirname, "../arrow-frontend/build/index.html"));
 });
 
 connectDB().then(() => {
